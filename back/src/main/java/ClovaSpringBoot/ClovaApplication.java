50c2368153d11677cc153f3e00ed35179bfceb72
yunhy96 <yhy0818@gmail.com>
2023-12-15 23:46:28 +0900

    프론트 리다이랙트

--- back/src/main/java/ClovaSpringBoot/ClovaApplication.java
+++ back/src/main/java/ClovaSpringBoot/ClovaApplication.java
@@ -9,5 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class ClovaApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClovaApplication.class, args);
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
     }
 }
