d804e9f104c76dd69907fb8bfc3d72075e0573fe
yunhy96 <yhy0818@gmail.com>
2023-12-17 04:09:37 +0900

    dd

--- back/src/main/java/ClovaSpringBoot/controller/LoginController.java
+++ back/src/main/java/ClovaSpringBoot/controller/LoginController.java
@@ -1,14 +1,23 @@
 package ClovaSpringBoot.controller;
 
+import ClovaSpringBoot.domain.User;
+import ClovaSpringBoot.dto.PlanWithDetailRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/getUserInfo")
-    public String redirectToMySchedule() {
-        return "dd";
-    }
+    //그룹 플랜 삭제 + 해당 플랜 재생성
+//    @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
+//    @PostMapping("/api/plans/create-multiple-and-delete")
+//    public ResponseEntity<String> createMultiplePlansAndDeleteByGroupId(@AuthenticationPrincipal Authentication authentication) {
+//        Long userId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        User user = userRepository.findById(userId).orElseThrow(IllegalArgumentException::new);
 }
--- back/src/main/java/ClovaSpringBoot/repository/PlanRepository.java
+++ back/src/main/java/ClovaSpringBoot/repository/PlanRepository.java
@@ -2,10 +2,14 @@ package ClovaSpringBoot.repository;
 
 import ClovaSpringBoot.domain.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface PlanRepository extends JpaRepository<Plan, Long> {
     List<Plan> findByGroupid(Long groupid);
     List<Plan> findByUserId(Long userId);
+    // groupId를 기반으로 GroupPlan을 삭제하는 메서드 직접 구현
+    @Transactional
+    void deletePlansByGroupid(Long groupid);
 }
