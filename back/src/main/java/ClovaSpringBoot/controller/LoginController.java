42663a733f5969c34fb832ece729252db1cb0a44
yunhy96 <yhy0818@gmail.com>
2023-12-17 02:36:25 +0900

    d

--- back/src/main/java/ClovaSpringBoot/controller/LoginController.java
+++ back/src/main/java/ClovaSpringBoot/controller/LoginController.java
@@ -5,11 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class RedirectController {
+public class LoginController {
 
-    @RequestMapping("/login")
+    @GetMapping("/getUserInfo")
     public String redirectToMySchedule() {
-        System.out.println("여기까지는 탐");
-        return "redirect:https://d1zdvff23sqy4w.cloudfront.net/mySchedule";
+        return "dd";
     }
 }
